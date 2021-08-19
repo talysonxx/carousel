e34746acde28619552323698bc5f06391c9819f1
talysonxx <talysonoficial12@gmail.com>
2021-08-19 18:10:06 -0300

    resultado final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import './App.css'
 import logoImg from './static/img/logo.png'
-import camisaUm from './static/img/camisa-1.jpg'
 import tos from './static/tos.json'
 
 
 export default function App() {
   const [data, setData] = useState([])
+  const carousel = useRef(null)
 
   useEffect(() => {
     document.title = 'React - Carousel'
@@ -18,14 +18,36 @@ export default function App() {
     // .catch(error => console.log('erro:', error))
   })
 
+  const handleLeftClick =  (event) => {
+    event.preventDefault()
+
+    console.log(carousel.current.offsetWidth)
+
+    carousel.current.scrollLeft -= carousel.current.offsetWidth / 3
+  }
+  const handleRightClick = (event) => {
+    event.preventDefault()
+
+    console.log(carousel.current.offsetWidth)
+
+    carousel.current.scrollLeft += carousel.current.offsetWidth / 3
+  }
+
   // if(!data || !data.length) return null
+
+  // pergunta: tem item lá?
+  if(!tos){
+    alert('Arquivos faltando! Não foi possível renderizar a página!')
+    return null
+  }
 
   return (
     <div className='container'>
       <div>
         <img className='logo' src={logoImg} alt='The Otaku Store Logo'/>
       </div>
-      <div className='carousel'>
+
+      <div className='carousel' ref={carousel}>
       {tos.map(({id, name, oldPrice, currentPrice, link}) => {
         return(
         <div className='item' key={id}>
@@ -39,9 +61,13 @@ export default function App() {
         )
       })}
       </div>
-        {/* font awasome */}
-        {/* <i class="fas fa-arrow-right"></i>
-        <i class="fas fa-arrow-left"></i> */}
+      <div className='buttons'>
+        <button onClick={handleLeftClick}><i className="fas fa-arrow-left"></i></button>
+        <button onClick={handleRightClick}><i className="fas fa-arrow-right"></i></button>
+      </div>
+      {/* font awasome */}
+      {/* <i class="fas fa-arrow-right"></i>
+      <i class="fas fa-arrow-left"></i> */}
     </div>
   )
 }

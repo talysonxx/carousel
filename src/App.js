e8897891cc2602fa2f748016a6468a659ccc8111
talysonxx <talysonoficial12@gmail.com>
2021-08-19 16:16:28 -0300

    estilizacao e package json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, {useEffect} from 'react'
+import './App.css'
+import logoImg from './img/logo.png'
+import camisaUm from './img/camisa-1.jpg'
 
 
 export default function App() {
@@ -7,11 +10,23 @@ export default function App() {
   })
 
   return (
-    <>
-      <span style={{fontSize: '2rem'}}>
-        <i class="fas fa-arrow-right"></i>
-        <i class="fas fa-arrow-left"></i>
-      </span>
-    </>
+    <div className='container'>
+      <div>
+        <img className='logo' src={logoImg} alt='The Otaku Store Logo'/>
+      </div>
+      <div className='carousel'>
+        <div className='item'>
+          <img className='itemImage' src={camisaUm}/>
+          <div className='itemInfo'>
+            <span className='itemName'>Nome do item</span>
+            <span className='itemOldPrice'>R$ 200,20</span>
+            <span className='itemCurrentPrice'>R$ 198,40</span>
+          </div>
+        </div>
+      </div>
+        {/* font awasome */}
+        {/* <i class="fas fa-arrow-right"></i>
+        <i class="fas fa-arrow-left"></i> */}
+    </div>
   )
 }

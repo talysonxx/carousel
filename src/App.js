dd15f97b84fa8d1c335eb51aa0a30b91e9cbb06b
talysonxx <talysonoficial12@gmail.com>
2021-08-19 17:14:23 -0300

    tos.json feito e adicionado no carrossel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css'
-import logoImg from './img/logo.png'
-import camisaUm from './img/camisa-1.jpg'
+import logoImg from './static/img/logo.png'
+import camisaUm from './static/img/camisa-1.jpg'
+import tos from './static/tos.json'
 
 
 export default function App() {
+  const [data, setData] = useState([])
+
   useEffect(() => {
     document.title = 'React - Carousel'
+
+    // fetch('http://localhost:3000/static/tos.json')
+    // // .then(console.log)
+    // .then(response => response.json())
+    // .then(setData)
+    // .catch(error => console.log('erro:', error))
   })
+
+  // if(!data || !data.length) return null
 
   return (
     <div className='container'>
@@ -15,14 +26,18 @@ export default function App() {
         <img className='logo' src={logoImg} alt='The Otaku Store Logo'/>
       </div>
       <div className='carousel'>
-        <div className='item'>
-          <img className='itemImage' src={camisaUm}/>
+      {tos.map(({id, name, oldPrice, currentPrice, link}) => {
+        return(
+        <div className='item' key={id}>
+          <img className='itemImage' src={link}/>
           <div className='itemInfo'>
-            <span className='itemName'>Nome do item</span>
-            <span className='itemOldPrice'>R$ 200,20</span>
-            <span className='itemCurrentPrice'>R$ 198,40</span>
+            <span className='itemName'>{name}</span>
+            <span className='itemOldPrice'>R$ {oldPrice}</span>
+            <span className='itemCurrentPrice'>R$ {currentPrice}</span>
           </div>
         </div>
+        )
+      })}
       </div>
         {/* font awasome */}
         {/* <i class="fas fa-arrow-right"></i>
